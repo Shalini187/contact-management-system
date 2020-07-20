d5ed5ef9c60713301a78665c918d0072ab31e6a0
Shalini Bhousle <vicinoti83@gmail.com>
2020-07-20 11:52:52 +0530

    Complete setup of React Contact Management System!!!!!

--- backend/routes/new.js
+++ backend/routes/new.js
@@ -1,29 +1,9 @@
 var express = require('express');
 const router = express.Router();
-const multer = require('multer');
-
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
-  }
-});
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  }
-});
-
-
 let New = require('../models/database.model');
 
 
-router.route('/create').post(upload.single('image'),(req, res) => {
+router.route('/create').post((req, res) => {
     const username = req.body.username;
     const phonenumber = Number(req.body.phonenumber);
     const email = req.body.email;
